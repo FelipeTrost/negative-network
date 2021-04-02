7efcc369d014415918da3cb38251269973158d01
Felipe Trost <felipe.trost@gmail.com>
2021-04-03 00:03:27 +0200

    Added giphy to post comment

--- frontend/src/UploadForm.js
+++ frontend/src/UploadForm.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import {  Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import {  Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Typography } from '@material-ui/core';
+import { SearchContextManager } from "@giphy/react-components";
+
+import GiphyModal from './GiphyModal';
 import client from './feathers';
 
 function UploadForm({modal, setModal, name, setName, own, setOwn}) {
   const [message, setMessage] = useState("");
   const [imageField, setImageField] = useState("");
+  const [giphyModal, setGiphyModal] = useState("");
 
   const upload = async () => {
     if(!message || !name) {
@@ -22,52 +26,77 @@ function UploadForm({modal, setModal, name, setName, own, setOwn}) {
   }
 
   return (
-    <Dialog open={modal} onClose={() => setModal(false)} aria-labelledby="add-comment">
-        <DialogTitle id="form-dialog-title">Be stupid</DialogTitle>
+      <>
+        <Dialog open={modal} onClose={() => setModal(false)} aria-labelledby="add-comment">
+            <DialogTitle id="form-dialog-title">Be stupid</DialogTitle>
 
-        <DialogContent>
-            {/* <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-            </DialogContentText> */}
-            <TextField
-                autoFocus
-                margin="dense"
-                label="Name"
-                type="text"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                fullWidth
-            />
+            <DialogContent>
+                {/* <DialogContentText>
+                To subscribe to this website, please enter your email address here. We will send updates
+                occasionally.
+                </DialogContentText> */}
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Name"
+                    type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    fullWidth
+                />
 
-            <TextField
-                label="Comment"
-                margin="dense"
-                // multiline
-                fullWidth
-                rows={4}
-                value={message}
-                onChange={e => setMessage(e.target.value)}
-            />
+                <TextField
+                    label="Comment"
+                    margin="dense"
+                    // multiline
+                    fullWidth
+                    rows={4}
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                />
 
-            <TextField
-                margin="dense"
-                label="Image link (optional)"
-                type="text"
-                value={imageField}
-                onChange={e => setImageField(e.target.value)}
-                fullWidth
-            />
+                <Grid
+                    direction="row"
+                    wrap="nowrap"
+                    alignItems="baseline"
+                    justify="space-between"
+                    container
+                >
+                    <TextField
+                        margin="dense"
+                        label="Image link (optional)"
+                        type="text"
+                        value={imageField}
+                        onChange={e => setImageField(e.target.value)}
+                        style={{width: "60%"}}
+                    />
 
-            <img className="comment-image" src={imageField} alt={imageField && "Not found"} />
-        </DialogContent>
+                    <Typography>Or</Typography>
 
-        <DialogActions>
-            <Button onClick={() => setModal(false)} color="primary">Cancel</Button>
+                    <Button
+                        onClick={() => setGiphyModal(true)}
+                        style={{width: "20%"}}
+                        variant="contained"
+                        color="secondary"
+                    >
+                        Giphy
+                    </Button>
+                </Grid>
 
-            <Button onClick={upload} color="secondary">Noooo</Button>
-        </DialogActions>
-    </Dialog>
+                <img className="comment-image" src={imageField} alt={imageField && "Not found"} />
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={() => setModal(false)} color="primary">Cancel</Button>
+
+                <Button onClick={upload} color="secondary">Noooo</Button>
+            </DialogActions>
+        </Dialog>
+
+        <SearchContextManager apiKey={"sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh"}>
+            <GiphyModal open={giphyModal} close={() => setGiphyModal(false)} setImage={setImageField} />
+        </SearchContextManager>
+    </>
   );
 }
 

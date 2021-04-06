071efe16230dc090095f2976dfc5695ab4c73fd2
Felipe Trost <felipe.trost@gmail.com>
2021-04-07 01:37:42 +0200

    uploaded something from local dev, oops

--- frontend/src/feathers.js
+++ frontend/src/feathers.js
@@ -2,9 +2,9 @@ import io from 'socket.io-client';
 import feathers from '@feathersjs/client';
 const rx = require('feathers-reactive');
 
-// const server = "https://negativitynetwork.herokuapp.com"
+const server = "https://negativitynetwork.herokuapp.com"
 
-const socket = io('http://localhost:3030');
+const socket = io(server);
 const client = feathers().configure(rx({
     idField: '_id'
   }));

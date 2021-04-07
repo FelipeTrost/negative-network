3dbb44f89af49d74e397006b5b3797e0d93647e0
Felipe Trost <felipe.trost@gmail.com>
2021-04-07 06:22:34 +0200

    fixed info button

--- frontend/src/FaqButton.js
+++ frontend/src/FaqButton.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import InfoIcon from '@material-ui/icons/Info';
-import { IconButton, Popover, Typography, Grid } from '@material-ui/core';
+import { IconButton, Popover, Typography } from '@material-ui/core';
 
 function FaqButton({ classes }) {
   const [infoAnchor, setInfoanchor] = useState(null)
 
   return (
     <>
-        <IconButton style={{backgroundColor:'transparent'}} sonClick={e => setInfoanchor(e.target)}> <InfoIcon /> </IconButton>
+        <IconButton style={{backgroundColor:'transparent'}} onClick={e => setInfoanchor(e.target)}> <InfoIcon /> </IconButton>
 
         <Popover
             id='mouse-over-popover'

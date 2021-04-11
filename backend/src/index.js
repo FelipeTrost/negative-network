4601a57aa92041c0bf690404ddaf9824eae7f397
Felipe Trost <felipe.trost@gmail.com>
2021-04-11 04:40:06 +0200

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,12 @@ mongoose.connect('mongodb+srv://app:3gbgCBtsZLVcHYv@cluster0.kspsb.mongodb.net/n
 .then(() => {
   const server = app.listen(port);
 
-  process.on('unhandledRejection', (reason, p) =>
-    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  server.on('listening', () =>
+    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
   );
 })
 
-
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
+

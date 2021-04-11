363e7a498f609276917da69b54c069f0f3860c6d
Felipe Trost <felipe.trost@gmail.com>
2021-04-11 04:32:43 +0200

    port: 5000

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY backend/ .
 
-EXPOSE 3333
+EXPOSE 5000
 
 CMD node src index.js
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,17 @@
 /* eslint-disable no-console */
-const mongoose = require('mongoose');
 const logger = require('./logger');
 const app = require('./app');
-const port = 3333;
+const port = app.get('port');
 mongoose.connect('mongodb+srv://app:3gbgCBtsZLVcHYv@cluster0.kspsb.mongodb.net/negativeNetwork?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
   const server = app.listen(port);
 
-  server.on('listening', () =>
-    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
   );
 })
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
 
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
